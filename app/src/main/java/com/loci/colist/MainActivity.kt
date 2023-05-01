acba6ce6c060f31c29003bbe26087ce0ad5ac01d
kdbswo <kdbswo@gmail.com>
2023-05-01 20:04:21 +0900

    add Timber

--- app/src/main/java/com/loci/colist/MainActivity.kt
+++ app/src/main/java/com/loci/colist/MainActivity.kt
@@ -2,10 +2,19 @@ package com.loci.colist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "onCreate")
+        Timber.d("onCreate")
+        Timber.tag("MainActivity").d("onCreate")
+        Timber.tag("MainActivity").e("onCreate")
     }
+
 }
