f216f6c6e840ae7f93c8e4533ed2ab56332d8c14
kdbswo <kdbswo@gmail.com>
2023-05-02 01:57:57 +0900

    add Intro activity

--- app/src/main/java/com/loci/colist/IntroActivity.kt
+++ app/src/main/java/com/loci/colist/IntroActivity.kt
@@ -2,16 +2,17 @@ package com.loci.colist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
 
         Timber.d("onCreate")
-
     }
-
 }
