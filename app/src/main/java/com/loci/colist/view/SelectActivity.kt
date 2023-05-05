2605786f82725adc47acb12a18e8e29eab2dfa6e
kdbswo <kdbswo@gmail.com>
2023-05-06 06:09:50 +0900

    Add RecyclerView in SelectActivity

--- app/src/main/java/com/loci/colist/view/SelectActivity.kt
+++ app/src/main/java/com/loci/colist/view/SelectActivity.kt
@@ -1,25 +1,45 @@
 package com.loci.colist.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import com.loci.colist.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.loci.colist.MainActivity
+import com.loci.colist.databinding.ActivitySelectBinding
+import com.loci.colist.view.adapter.SelectRVAdapter
 import timber.log.Timber
 
 // https://api.bithumb.com/public/ticker/ALL_KRW \
 class SelectActivity : AppCompatActivity() {
 
-    private val viewModel : SelectViewModel by viewModels()
+    private lateinit var binding: ActivitySelectBinding
+
+    private val viewModel: SelectViewModel by viewModels()
+
+    private lateinit var selectRVAdapter: SelectRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getCurrentCoinList()
-        viewModel.currentPriceResult.observe(this, Observer{
+        viewModel.currentPriceResult.observe(this, Observer {
+
+            selectRVAdapter = SelectRVAdapter(this, it)
+
+            binding.coinListRV.adapter = selectRVAdapter
+            binding.coinListRV.layoutManager = LinearLayoutManager(this)
+
             Timber.d(it.toString())
         })
+
+        binding.laterTextArea.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
