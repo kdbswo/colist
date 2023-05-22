cab33a7e50e42ee3efff66943763e35d51b07b3a
kdbswo <kdbswo@gmail.com>
2023-05-23 00:38:26 +0900

    Add Notification to service

--- app/src/main/java/com/loci/colist/view/main/MainActivity.kt
+++ app/src/main/java/com/loci/colist/view/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.loci.colist.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.loci.colist.R
 import com.loci.colist.databinding.ActivityMainBinding
+import com.loci.colist.view.setting.SettingActivity
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.setting.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
 
         val bottomNavigationView = binding.bottomNavigationView
         val navController = findNavController(R.id.fragmentContainerView)
